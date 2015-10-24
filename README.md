1d3d5abe00eeedf073329a66c8b41ec0f561c623
James Skemp <strivinglife@gmail.com>
2015-10-24 13:57:52 -0500

    Created small template, using JS prototypes for the game.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Phaser-Project-Templates
+
+The Phaser logo (Phaser-Logo-Small.png) is a trademark of Photon Storm, and is meant to be used in a manner consistent with their [usage guidelines](http://phaser.io/download/trademark).
---
+++ Small-Prototype/src/game.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../lib/phaser-2.4.4.js" />
+var SmallGame = {};
+
+SmallGame.Game = function (game) {
+	this.phaserLogo = null;
+	this.phaserLogoText = null;
+};
+
+SmallGame.Game.prototype = {
+	preload: function () {
+		this.load.path = 'assets/';
+		this.load.image('Phaser-Logo-Small');
+	},
+
+	create: function () {
+		// 382, 331
+		this.phaserLogo = this.add.sprite(this.world.centerX, this.world.centerY, 'Phaser-Logo-Small');
+		this.phaserLogo.anchor.setTo(0.5);
+
+		this.phaserLogoText = this.add.text(16, 16, "Powered by", { fill: '#fff', fontSize: '24px' });
+	},
+
+	update: function () {
+
+	}
+};
